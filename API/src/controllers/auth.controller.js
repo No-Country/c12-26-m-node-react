130c6df8627dc566b56c6df1b713ca9c3f961678
Admin <christian.herrera.onaindi@gmail.com>
2023-07-05 16:01:37 -0400

    SignIn and profile endpoint create

--- API/src/controllers/auth.controller.js
+++ API/src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 const prisma = require('../db')
-const {encryptPassword} = require('../utils/passwordHash');
+const {encryptPassword, comparePassword} = require('../utils/passwordHash')
+const jwt = require('jsonwebtoken')
+const tokenSecret = process.env.TOKEN_SECRET
+const tokenExp = process.env.TOKEN_EXP
 
 const signUp = async (req, res) =>{
     const {firstName, secondName, lastName, secondLastName, 
@@ -42,4 +45,48 @@ const signUp = async (req, res) =>{
     }
 }
 
-module.exports = {signUp}
+const signIn = async (req, res) =>{
+    const {email, password} = req.body
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+        if (!user) return res.status(404).json({"message": "email_not_found"})
+        const passwordValid = await comparePassword(password, user.password)
+        if (!passwordValid) return res.status(404).json({"message": "password_incorrect"})
+        const token = jwt.sign({id: user.id}, tokenSecret, {expiresIn: '24h'})
+        res.header('Authorization', token).json(user)
+    } catch (error) {
+        res.status(500).json({"message": "Error"})
+        console.log(error)
+    }
+}
+
+const profile = async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.userId // Cargado en validateToken
+            },
+            select: { // User sin exponer el password
+                id: true,
+                first_name: true,
+                second_name: true,
+                last_name: true,
+                second_last_name: true,
+                documentID: true,
+                country: true,
+                email: true,
+                profile_img: true
+              }
+        })
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({"message": "Error"})
+        console.log(error)
+    }
+}
+
+module.exports = {signUp, signIn, profile}
--- API/src/routes/auth.routes.js
+++ API/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
-const authController = require('../controllers/auth.controller')
+const {signIn, signUp, profile} = require('../controllers/auth.controller')
 const {Router} = require('express')
+const validateToken = require('../middleware/validateToken')
 
 const router = Router()
 
-router.post('/signup', authController.signUp)
-
+router.post('/signup', signUp)
+router.post('/signin', signIn)
+router.get('/profile', validateToken , profile)
 module.exports = router
