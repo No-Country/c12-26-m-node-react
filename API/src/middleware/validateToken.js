9195aca9d5fc5437830c6c0587a84e81af6ea992
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-07-12 02:03:04 -0400

    Update models/data structure/validations

--- API/src/middleware/validateToken.js
+++ API/src/middleware/validateToken.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken')
-const tokenSecret = process.env.TOKEN_SECRET
+const jwt = require("jsonwebtoken");
+const tokenSecret = process.env.TOKEN_SECRET;
 
-const validateToken= (req, res, next) =>{
-    const token = req.headers.authorization
-    if(!token) return res.status(401).json({"message": "token_missing"})
-    try {
-        const token_decoded = jwt.verify(token, tokenSecret)
-        req.userId = token_decoded.id
-        next()
-    } catch (error) {
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({"message": "token_expired"});
-          }
-        console.log(error)
-        res.status(500).json({"message": "error"})
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: "token_missing" });
+  try {
+    const token_decoded = jwt.verify(token, tokenSecret);
+    req.userId = token_decoded.id;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "token_expired" });
     }
-}
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+};
 
-module.exports = validateToken
+module.exports = validateToken;
--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -1,5 +1,11 @@
 const Router = require("express");
 
+const userRouter = require("./userRouter");
+const loginRouter = require("./loginRouter");
+
 const router = Router();
+
+router.use("/users", userRouter);
+router.use("/login", loginRouter);
 
 module.exports = router;
