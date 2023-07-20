5678a52b0859577892aaf6b869a16214a72d2556
Delwin183 <90370429+Delwin183@users.noreply.github.com>
2023-07-20 03:57:59 -0400

    Fix-Stripe-Funtion

--- FrontEnd/src/components/Stripe/Payment.jsx
+++ FrontEnd/src/components/Stripe/Payment.jsx
@@ -1,51 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Elements } from "@stripe/react-stripe-js";
-import Input from "../Layouts/Input"
-import { useForm } from 'react-hook-form'
-import {loadStripe} from '@stripe/stripe-js';
-import axios from 'axios';
-import CheckoutForm from './CheckoutForm';
-import { useSelector } from 'react-redux';
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import CheckoutForm from "./CheckoutForm";
+import { useSelector } from "react-redux";
 
-export default function Payment () {
-  const [clientSecret, setClientSecret] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const { register, formState: {errors} } = useForm({defaultValues: {
-    amount :""
-}})
-const  user  = useSelector((state) => state.user)
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+export default function Payment() {
+  const [clientSecret, setClientSecret] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useSelector((state) => state.user);
+  const [amount, setAmount] = useState(0);
+
   useEffect(() => {
-    axios.post(`${process.env.REACT_APP_API_URL}payment/create-payment-intent`, {
-      headers: {
-        'Content-Type': "application/json",
-        'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWViNTcyYWI2NDM4NWI3MDcyZjg0YSIsImlhdCI6MTY4OTE4NjA0NCwiZXhwIjoxNjkwMzk1NjQ0fQ.BhXK7waVndPBlt3OOITOin9Fxtp7b0l4arJ2hiJnU3c",
-      }
-    }).then(async (result) => {
-        console.log(result)
-    });
-  }, [user.user.payload.token]);
+    // Obtener el clientSecret al cargar el componente
+    if (user?.payload?.token && amount > 0) {
+      axios
+        .post(
+          `${process.env.REACT_APP_API_URL}payment/create-payment-intent`,
+          {
+            amount: amount,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: user.payload.token,
+            },
+          },
+        )
+        .then((result) => {
+          setClientSecret(result.data.clientSecret);
+        })
+        .catch((error) => {
+          console.error("Error al obtener el clientSecret:", error);
+        });
+    }
+  }, [user?.payload?.token, amount]);
 
   return (
     <div className="mt-[200px]">
-      <div className='bg-white p-4 rounded-xl'>
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700 undefined"> Transfer Amount </label>
-                    <div className="flex flex-col items-start">
-                        <Input
-                            type={"number"}
-                            id={"amount"}
-                            register={register}
-                            disabled={isLoading}
-                            errors={errors}
-                        />
-                    </div>
-                </div>
-      {clientSecret && stripePromise && (
-        <Elements stripe={stripePromise} options={{ clientSecret }}>
-          <CheckoutForm />
+      <div className="bg-white p-4 rounded-xl mb-4">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Transfer Amount
+        </label>
+        <div className="flex flex-col items-start">
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            disabled={isLoading}
+            min="0" // Asegurar que el monto no sea negativo
+            inputMode="numeric" // Permitir solo entradas numéricas
+            className="w-full mt-1 py-2 px-3 bg-white border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600"
+          />
+        </div>
+      </div>
+      {clientSecret && amount > 0 && stripePromise && (
+        <Elements stripe={stripePromise}>
+          <div className="bg-white p-4 rounded-xl">
+            <label
+              htmlFor="card-element"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Card Information
+            </label>
+            <div className="mt-1" id="card-element">
+              <CheckoutForm options={{ clientSecret }} />
+            </div>
+          </div>
         </Elements>
       )}
     </div>
   );
-
 }
--- FrontEnd/src/components/UserAuth/Login.jsx
+++ FrontEnd/src/components/UserAuth/Login.jsx
@@ -1,97 +1,129 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import Skeleton from 'react-loading-skeleton'
-import axios from "axios"
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Skeleton from "react-loading-skeleton";
+import axios from "axios";
 import toast from "react-hot-toast";
-import Input from '../Layouts/Input';
-import { useDispatch } from 'react-redux';
-import {getCurrentUser} from "../../hooks/CurrentUserSlice"
-import { closeModal } from '../../hooks/modalSlice';
-import { useNavigate } from 'react-router-dom';
+import Input from "../Layouts/Input";
+import { useDispatch } from "react-redux";
+import { getCurrentUser } from "../../hooks/CurrentUserSlice";
+import { closeModal } from "../../hooks/modalSlice";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const [isLoading, setIsLoading] = useState(false);
-    const { register, handleSubmit, formState: {errors}, reset } = useForm({defaultValues: {
-        email: "",
-        password: "",
-    }})
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const onSubmit = (data) => {
-        setIsLoading(true);
-          console.log(data)
-        axios.post(`${process.env.REACT_APP_API_URL}login`, data)
-        .then((response) => {
-          toast.success(response.data.message);
-          console.log(response)
-          dispatch(getCurrentUser(response.data))
-          reset()
-          navigate('/main');
-        })
-        .catch((error) => { 
-          toast.error(error);
-        })
-        .finally(() => {
-          setIsLoading(false);
-          
-        })
-      
-      };
+  const onSubmit = (data) => {
+    setIsLoading(true);
+    console.log(data);
+    axios
+      .post(`${process.env.REACT_APP_API_URL}login`, data)
+      .then((response) => {
+        const responseData = response.data; // Obtiene el objeto completo de la respuesta
+        toast.success(responseData.message); // Muestra el mensaje de éxito o error utilizando solo la propiedad "message"
+        console.log(responseData);
+        dispatch(getCurrentUser(responseData)); // Actualiza el estado del usuario
+        reset();
+        navigate("/main");
+      })
+      .catch((error) => {
+        toast.error("An unexpected error occurred."); // Mostrar un mensaje genérico si ocurre un error
+        console.error("Error de inicio de sesión:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
   return (
     <div>
-    
- <div className="w-full px-6 py-4 mt-[300px] overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
-            <form onSubmit={handleSubmit(onSubmit)}>      
-                <div className="mt-4">
-                    <label htmlFor="email" className="block text-sm font-medium text-gray-700 undefined"> Email </label>
-                    <div className="flex flex-col items-start">
-                        <Input
-                            type={"email"}
-                            id={"email"}
-                            register={register}
-                            disabled={isLoading}
-                            errors={errors}
-                        />
-                    </div>
-                </div>
-                <div className="mt-4">
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700 undefined"> Password </label>
-                    <div className="flex flex-col items-start">
-                        <Input
-                            type={"password"}
-                            id={"password"}
-                            register={register}
-                            disabled={isLoading}
-                            errors={errors}
-                        />
-                    </div>
-                </div>
-                <a href="about_blank" className="text-xs text-purple-600 hover:underline"> Forget Password?</a>
-                <div className="flex items-center mt-4">
-                    <button onClick={handleSubmit(onSubmit)} className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                        Login
-                    </button>
-                </div>
-            </form>
-            <div className="mt-4 text-grey-600">
-                Don't have an account?{" "}
-                <span>
-                    <button onClick={() => dispatch(closeModal())} useNavigate className="text-purple-600 hover:underline" href="about_blank">
-                        Register
-                    </button>
-                </span>
+      <div className="w-full px-6 py-4 mt-[300px] overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700 undefined"
+            >
+              {" "}
+              Email{" "}
+            </label>
+            <div className="flex flex-col items-start">
+              <Input
+                type={"email"}
+                id={"email"}
+                register={register}
+                disabled={isLoading}
+                errors={errors}
+              />
             </div>
-            <div className="flex items-center w-full my-4">
-                <hr className="w-full" />
-                <p className="px-3 ">OR</p>
-                <hr className="w-full" />
+          </div>
+          <div className="mt-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700 undefined"
+            >
+              {" "}
+              Password{" "}
+            </label>
+            <div className="flex flex-col items-start">
+              <Input
+                type={"password"}
+                id={"password"}
+                register={register}
+                disabled={isLoading}
+                errors={errors}
+              />
             </div>
-            <div className="my-6 space-y-2">
-                <Skeleton />
-                <Skeleton />
-            </div>
+          </div>
+          <a
+            href="about_blank"
+            className="text-xs text-purple-600 hover:underline"
+          >
+            {" "}
+            Forget Password?
+          </a>
+          <div className="flex items-center mt-4">
+            <button
+              onClick={handleSubmit(onSubmit)}
+              className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="mt-4 text-grey-600">
+          Don't have an account?{" "}
+          <span>
+            <button
+              onClick={() => dispatch(closeModal())}
+              useNavigate
+              className="text-purple-600 hover:underline"
+              href="about_blank"
+            >
+              Register
+            </button>
+          </span>
         </div>
+        <div className="flex items-center w-full my-4">
+          <hr className="w-full" />
+          <p className="px-3 ">OR</p>
+          <hr className="w-full" />
+        </div>
+        <div className="my-6 space-y-2">
+          <Skeleton />
+          <Skeleton />
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
